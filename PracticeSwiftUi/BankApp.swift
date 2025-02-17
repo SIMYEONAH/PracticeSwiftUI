49273129186b53e4ff67473f37d18568794e28c8
Yeonah Shim <simya0528@naver.com>
2025-02-17 22:21:09 +0900

    Button custom

--- PracticeSwiftUi/BankApp.swift
+++ PracticeSwiftUi/BankApp.swift
@@ -8,10 +8,45 @@
 import SwiftUI
 
 struct BankApp: View {
+    @State private var selectedTab = 0
+    @State private var cardOffset: CGFloat = 0
+    @State private var showDetails = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                
+            }
+        }
     }
 }
+
+
+struct ActionButton: View {
+    var iconName: String
+    var labels: String
+    var body: some View {
+        VStack {
+            Button {
+                
+            } label: {
+                Image(systemName: iconName)
+                    .font(.title)
+                    .foregroundColor(.white)
+                    .padding()
+                    .background(LinearGradient(gradient: Gradient(colors: [Color.blue.opacity(0.7), Color.purple.opacity(0.7)]), startPoint: .topLeading, endPoint: .bottomTrailing))
+                    .clipShape(Circle())
+            }
+            
+            Text(labels)
+                .font(.caption)
+                .foregroundStyle(.gray)
+        }
+    }
+}
+
+
+
 
 #Preview {
     BankApp()
