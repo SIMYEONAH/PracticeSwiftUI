7e2dcfd0048f37db1a3ed9cc1f2070d8294e4fad
Yeonah Shim <simya0528@naver.com>
2025-02-17 22:04:15 +0900

    BankApp Start

---
+++ PracticeSwiftUi/BankApp.swift
@@ -0,0 +1,18 @@
+//
+//  BankApp.swift
+//  PracticeSwiftUi
+//
+//  Created by 심연아 on 2/17/25.
+//
+
+import SwiftUI
+
+struct BankApp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BankApp()
+}
